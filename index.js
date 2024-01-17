23b96f86a541fadebf1014e9babdcff35f3d5341
Shyamal Das <shyamald934@gmail.com>
2024-01-17 16:24:37 +0530

    Test 4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const socket = require("socket.io");
 const path = require('path');
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
