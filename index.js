307b47d3b8e78a7e6ebcf06d2cc47c6561fb1b9d
Shyamal Das <shyamald934@gmail.com>
2024-01-17 14:32:08 +0530

    Ready to Deploy !

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+const path = require('path');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+// db connection
+main().catch(err=> console.log(err));
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('Database connected')
+}
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
